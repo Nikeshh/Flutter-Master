a71ff6d44b6ec66177130d735e1f43b41f396352
nikeshhbaskaran01@gmail.com <nikeshhbaskaran01@gmail.com>
2022-02-01 14:08:15 -0500

    Home page changes

--- Miscellaneous/cubit - inprogress/Project/cubit/lib/pages/navpages/home_page.dart
+++ Miscellaneous/cubit - inprogress/Project/cubit/lib/pages/navpages/home_page.dart
@@ -120,30 +120,34 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
               itemCount: 4,
               scrollDirection: Axis.horizontal,
               itemBuilder: (_, index) {
-                return Column(
-                  children: [
-                    Container(
-                      // margin: const EdgeInsets.only(right: 50),
-                      width: 80,
-                      height: 80,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.white,
-                        image: DecorationImage(
-                          image: AssetImage(
-                            "img/mountain.jpeg"
-                          ),
-                          fit: BoxFit.cover,
-                        )
+                return Container(
+                  margin: const EdgeInsets.only(right: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        // margin: const EdgeInsets.only(right: 50),
+                        width: 80,
+                        height: 80,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.white,
+                          image: DecorationImage(
+                            image: AssetImage(
+                              "img/mountain.jpeg"
+                            ),
+                            fit: BoxFit.cover,
+                          )
+                        ),
                       ),
-                    ),
-                    Container(
-                      child: AppText(
-                        text: "Kayaking",
-                        color: AppColors.textColor2,
+                      Container(
+                        child: AppText(
+                          text: "Kayaking",
+                          color: AppColors.textColor2,
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 );
               }
             ),
