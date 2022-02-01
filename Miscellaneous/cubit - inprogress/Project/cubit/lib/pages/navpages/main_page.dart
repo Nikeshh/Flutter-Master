68b4c4ea77521df198f55e774e9706bfa4d0d88e
nikeshhbaskaran01@gmail.com <nikeshhbaskaran01@gmail.com>
2022-01-31 19:09:54 -0500

    Added pages and bottom navigation bar

---
+++ Miscellaneous/cubit - inprogress/Project/cubit/lib/pages/navpages/main_page.dart
@@ -0,0 +1,38 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({ Key? key }) : super(key: key);
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            title: Text("Home"),
+            icon: Icon(Icons.apps),
+          ),
+          BottomNavigationBarItem(
+            title: Text("Bar"),
+            icon: Icon(Icons.bar_chart_sharp),
+          ),
+          BottomNavigationBarItem(
+            title: Text("Search"),
+            icon: Icon(Icons.search),
+          ),
+          BottomNavigationBarItem(
+            title: Text("My"),
+            icon: Icon(Icons.person),
+          ),
+        ],
+      ),
+    );
+  }
+}
