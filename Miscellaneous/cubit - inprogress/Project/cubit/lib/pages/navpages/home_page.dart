4d1c099aea5c1ff0750b51a9b9bf906d6f4574e8
nikeshhbaskaran01@gmail.com <nikeshhbaskaran01@gmail.com>
2022-02-01 09:24:21 -0500

    Home page changes

--- Miscellaneous/cubit - inprogress/Project/cubit/lib/pages/navpages/home_page.dart
+++ Miscellaneous/cubit - inprogress/Project/cubit/lib/pages/navpages/home_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -14,9 +16,10 @@ class _HomePageState extends State<HomePage> {
       body: Column(
         children: [
           Container(
+            padding: const EdgeInsets.only(top: 70, left: 20),
             child: Row(
               children: [
-                
+                Icon(Icons.menu, size: 30, color: Colors.black54),
               ],
             ),
           ),
