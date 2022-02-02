d71e93b63fc0fb3fd05b929275d4c989c6e3ac65
nikeshhbaskaran01@gmail.com <nikeshhbaskaran01@gmail.com>
2022-02-02 11:16:38 -0500

    Detail Page changes

--- Miscellaneous/cubit - inprogress/Project/cubit/lib/widgets/app_buttons.dart
+++ Miscellaneous/cubit - inprogress/Project/cubit/lib/widgets/app_buttons.dart
@@ -1,15 +1,22 @@
 import 'package:cubit/misc/colors.dart';
+import 'package:cubit/widgets/app_text.dart';
 import 'package:flutter/material.dart';
 
 class AppButtons extends StatelessWidget {
 
   final Color color;
+  String? text;
+  IconData? icon;
   final Color backgroundColor;
   double size;
   final Color borderColor;
+  bool? isIcon;
 
   AppButtons({ 
     Key? key,
+    this.isIcon = false,
+    this.text = "Hi",
+    this.icon,
     required this.size,
     required this.color,
     required this.backgroundColor,
@@ -29,6 +36,9 @@ class AppButtons extends StatelessWidget {
         borderRadius: BorderRadius.circular(15),
         color: backgroundColor
       ),
+      child: isIcon == false ? 
+                Center(child: AppText(text: text!, color: Colors.black)) :
+                Center(child: Icon(icon, color: color)),
     );
   }
 }
