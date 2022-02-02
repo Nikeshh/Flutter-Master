8a1355e59e4f801c6c1de7a8aae3cb40bc029158
nikeshhbaskaran01@gmail.com <nikeshhbaskaran01@gmail.com>
2022-02-02 10:16:46 -0500

    Detail Page changes

--- Miscellaneous/cubit - inprogress/Project/cubit/lib/widgets/app_buttons.dart
+++ Miscellaneous/cubit - inprogress/Project/cubit/lib/widgets/app_buttons.dart
@@ -2,7 +2,19 @@ import 'package:cubit/misc/colors.dart';
 import 'package:flutter/material.dart';
 
 class AppButtons extends StatelessWidget {
-  const AppButtons({ Key? key }) : super(key: key);
+
+  final Color color;
+  final Color backgroundColor;
+  double size;
+  final Color borderColor;
+
+  AppButtons({ 
+    Key? key,
+    required this.size,
+    required this.color,
+    required this.backgroundColor,
+    required this.borderColor
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
